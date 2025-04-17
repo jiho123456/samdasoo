8a1cbb723ec883c9ecff9d1672e5f30aa9b47dd9
jiho123456 <116412480+jiho123456@users.noreply.github.com>
2025-04-17 21:07:25 +0000

    Added message

--- libs/ui_helpers.py
+++ libs/ui_helpers.py
@@ -3,4 +3,5 @@ import streamlit as st
 def header():
     st.image('assets/logo.png', width=250)
     st.title("🌊 5-9 삼다수반")
-    st.markdown("#### 왼쪽 메뉴에서 원하는 기능을 선택하세요.")
+    st.markdown("""#### 왼쪽 메뉴에서 원하는 기능을 선택하세요.
+                ##### 회원가입은 반드시 본인 이름으로 해주시기 바랍니다.""")
