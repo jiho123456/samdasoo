80f0400d7bf8f88f1118ac8faa9a59806c66bb61
jiho123456 <116412480+jiho123456@users.noreply.github.com>
2025-04-17 06:10:45 +0000

    FIX BUG

--- libs/db.py
+++ libs/db.py
@@ -2,7 +2,7 @@ import psycopg2, streamlit as st
 
 @st.cache_resource
 def get_conn():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         user=st.secrets["user"],
         password=st.secrets["password"],
         host=st.secrets["host"],
@@ -11,3 +11,5 @@ def get_conn():
         keepalives=1, keepalives_idle=30,
         keepalives_interval=10, keepalives_count=5
     )
+    conn.autocommit = True
+    return conn
